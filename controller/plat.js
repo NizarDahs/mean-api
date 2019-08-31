3fdbff3e69cae36fb0fd398f25afd66edf21910e
NizarDahs <nizardahs7@gmail.com>
2019-08-31 16:51:45 +0100

    delete+update+getId

--- controller/plat.js
+++ controller/plat.js
@@ -1,20 +1,22 @@
 var Plat = require('../models/plats');
+
+
 exports.getAllPlats = function (req, res) {
     Plat.find({}, (err, data) => {
         if (err) {
             res.status(500).json({
                 sucess: false,
                 error: err
-                
+
             })
-        }else {
-                res.status(200).json({
-                    sucess: true,
-                    plats:data
-                })
-            }
-        
-        
+        } else {
+            res.status(200).json({
+                sucess: true,
+                plats: data
+            })
+        }
+
+
     });
 }
 
@@ -31,13 +33,129 @@ exports.AddPlat = function (req, res) {
             res.status(500).json({
                 success: false,
                 error: 'cant add to database',
-                err:err
+                err: err
             })
         } else {
             res.status(200).json({
                 success: true,
-               new: plat
+                new: plat
             })
         }
     });
+}
+
+
+exports.getPlatById = function (req, res) {
+    Plat.findById(req.params.id, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                error: err,
+                message: 'can not access database'
+            })
+        }
+        else {
+            if (data) {
+                res.status(200).json({
+                    success: true,
+                    plat: data
+                })
+
+
+            }
+            else {
+                res.status(404).json({
+                    success: false,
+                    message: 'database not finded',
+                    plat: data
+                })
+            }
+        }
+    })
+}
+
+
+exports.updatePlat = function (req, res) {
+    Plat.findByIdAndUpdate(req.params.id, req.body, function (err) {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                message: 'cannot update',
+                error: err
+            })
+        }
+        else {
+            Plat.findById(req.params.id, (err, data) => {
+                if (err) {
+                    res.status(500).json({
+                        success: false,
+                        error: err,
+                        message: 'can not access database'
+                    })
+                }
+                else {
+                    if (data) {
+                        res.status(200).json({
+                            success: true,
+                            plat: data
+                        })
+
+
+                    }
+                    else {
+                        res.status(404).json({
+                            success: false,
+                            message: 'database not finded',
+                            plat: data
+                        })
+                    }
+                }
+            })
+        }
+    })
+
+}
+/* alt shift a */
+
+exports.deletePlat = function (req, res) {
+    Plat.findById(req.params.id, (err, data) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                error: err,
+                message: 'can not access database'
+            })
+        }
+        else {
+            if (data) {
+                //function remove
+                Plat.findById(req.params.id).remove((err, data) => {
+                    if (err) {
+                        res.status(500).json({
+                            success: false,
+                            error: err,
+                            message: 'cannot delete plat'
+                        })
+
+                    }
+                    else {
+                        res.status(200).json({
+                            success: true,
+                            message: 'plat deleted'
+                        })
+
+                    }
+                })
+
+
+            }
+            else {
+                res.status(404).json({
+                    success: false,
+                    message: 'plat for delete not found',
+
+                })
+            }
+        }
+    })
 }
--- routes/plats.js
+++ routes/plats.js
@@ -5,5 +5,8 @@ var platController = require('../controller/plat');
 
 router.get('/', platController.getAllPlats);
 router.post('/add', platController.AddPlat);
+router.get('/get/:id', platController.getPlatById);
+router.put('/update/:id', platController.updatePlat);
+router.delete('/delete/:id', platController.deletePlat);
 
 module.exports = router;
